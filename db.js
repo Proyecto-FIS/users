d5c0c8eeb45f857ede848ba71a129092ebd2497a
Arturo Perez <arturoperez.bs@gmail.com>
2020-12-14 19:55:52 +0100

    fix(db.js):added options to avoid deprecation warning

--- db.js
+++ db.js
@@ -4,7 +4,12 @@ const DB_URL = process.env.MONGO_URL || 'mongodb://localhost/test';
 const dbConnect = function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
-    return mongoose.connect(DB_URL, {useNewUrlParser: true});
+    return mongoose.connect(DB_URL, {
+        useNewUrlParser: true, 
+        useCreateIndex: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false 
+    });
 }
 
 
